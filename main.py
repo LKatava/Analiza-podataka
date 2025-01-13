cb475450f773be08a8b58241ce0ca2a59a7a21c0
Luka Katava <katavaluka@gmail.com>
2025-01-13 21:28:21 +0100

    Dataframeovi su manipulirani tako da imaju iste indexe zatim meltani za lakšu analizu i spojeni u jedan data frame

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import requests
 
 #Excel
 df_obrazovanje_srednje = pd.read_excel("obrazovanje.xlsx",sheet_name="8.1.3. ")
-df_obrazovanje_fakultet = pd.read_excel("obrazovanje.xlsx",sheet_name="8.1.4. ")
-df_obrazovanje_diplomirani = pd.read_excel("obrazovanje.xlsx",sheet_name="8.1.5. ")
+df_obrazovanje_fakultet = pd.read_excel("obrazovanje.xlsx",sheet_name="8.1.4.")
+df_obrazovanje_diplomirani = pd.read_excel("obrazovanje.xlsx",sheet_name="8.1.5.")
 
 df_obrazovanje_srednje=df_obrazovanje_srednje.drop('County of',axis=1)
 df_obrazovanje_fakultet=df_obrazovanje_fakultet.drop('County of',axis=1)
 df_obrazovanje_diplomirani=df_obrazovanje_diplomirani.drop('County of',axis=1)
 
 #Json
-url = "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/tgs00007?geo=HR02&geo=HR03&geo=HR05&geo=HR06&time=2021&time=2022&time=2023&format=JSON"
+url = "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/tgs00007?geo=HR02&geo=HR03&geo=HR05&geo=HR06&time=2012&time=2013&time=2014&time=2015&time=2016&time=2017&time=2018&time=2019&time=2020&time=2021&time=2022&time=2023&format=JSON"
 response = requests.get(url)
 data = response.json()
 
@@ -42,3 +42,67 @@ for key, value in values.items():
     })
 
 df_json = pd.DataFrame(rows)
+
+zupanija_u_regiju = {
+    "Zagrebačka" : "Sjeverna Hrvatska",
+    "Krapinsko-zagorska": "Sjeverna Hrvatska",
+    "Sisačko-moslavačka": "Panonska Hrvatska",
+    "Karlovačka": "Panonska Hrvatska",
+    "Varaždinska": "Sjeverna Hrvatska",
+    "Koprivničko-križevačka": "Sjeverna Hrvatska",
+    "Bjelovarsko-bilogorska": "Panonska Hrvatska",
+    "Primorsko-goranska": "Jadranska Hrvatska",
+    "Ličko-senjska": "Jadranska Hrvatska",
+    "Virovitičko-podravska": "Panonska Hrvatska",
+    "Požeško-slavonska": "Panonska Hrvatska",
+    "Brodsko-posavska": "Panonska Hrvatska",
+    "Zadarska": "Jadranska Hrvatska",
+    "Osječko-baranjska": "Panonska Hrvatska",
+    "Šibensko-kninska": "Jadranska Hrvatska",
+    "Vukovarsko-srijemska": "Panonska Hrvatska",
+    "Splitsko-dalmatinska": "Jadranska Hrvatska",
+    "Istarska": "Jadranska Hrvatska",
+    "Dubrovačko-neretvanska": "Jadranska Hrvatska",
+    "Međimurska": "Sjeverna Hrvatska",
+    "Grad Zagreb":"Grad Zagreb"
+}
+
+df_obrazovanje_srednje['regija'] = df_obrazovanje_srednje['Županija'].map(zupanija_u_regiju)
+df_grupirana_srednja = df_obrazovanje_srednje.groupby('regija').sum().reset_index()
+
+df_grupirana_srednja=df_grupirana_srednja.drop(columns="Županija")
+
+pivot_df = df_json.pivot(index="Region",columns="Year",values="Employment Rate")
+
+df_cut_srednja = df_grupirana_srednja.drop(columns={'2005./2006.', '2006./2007.', '2007./2008.', '2008./2009.',
+       '2009./2010.', '2010./2011.','2011./2012.'})
+
+df_cut_srednja.columns = [col.split('.')[0] for col in df_cut_srednja.columns]
+
+df_simple = pivot_df.reset_index()
+
+df_simple.columns = ['Region'] + [str(col) for col in df_simple.columns[1:]]
+
+df_students_melted = pd.melt(
+    df_cut_srednja, 
+    id_vars=['regija'], 
+    var_name='Godina', 
+    value_name='Broj Studenata'
+)
+
+# Unpivot employment dataframe
+df_employment_melted = pd.melt(
+    df_simple, 
+    id_vars=['Region'], 
+    var_name='Godina', 
+    value_name='Postotak Zaposlenosti'
+)
+
+# Prilagodba naziva stupaca za regiju
+df_employment_melted = df_employment_melted.rename(columns={'Region': 'regija'})
+
+df_combined = pd.merge(
+    df_students_melted, 
+    df_employment_melted, 
+    on=['regija', 'Godina']
+)
